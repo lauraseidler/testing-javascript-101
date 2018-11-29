eb9e36d68a213ba7e342141619b9c5c03132a2ef
Laura Seidler <laura.seidler@oetkerdigital.com>
2018-11-29 11:35:37 +0100

    Example 4.1: Form submitted message

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import App from '../App';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 describe('The App', () => {
     it('renders without crashing', () => {
         mount(<App />);
+    });
+
+    it('shows "Submitted!" message after login form submit', () => {
+        const app = shallow(<App />);
+
+        expect(app.find('LoginForm')).not.toBeNull();
+        expect(app.contains('Submitted!')).toBe(false);
+
+        app.instance().submitLoginForm({ preventDefault: () => null });
+
+        expect(app.contains('Submitted!')).toBe(true);
     });
 });
