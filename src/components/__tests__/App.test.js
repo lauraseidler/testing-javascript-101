b9cea114d61f086e6d2b9eb30982d1244c8e8912
Laura Seidler <laura.seidler@oetkerdigital.com>
2018-11-29 12:23:33 +0100

    Install react-testing-library

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../App';
+import { render } from 'react-testing-library';
 
 describe('The App', () => {
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        render(<App />);
     });
 });
