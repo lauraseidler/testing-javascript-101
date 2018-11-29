5c43b321b96f995d1384cef86d6ff7350313f8db
Laura Seidler <laura.seidler@oetkerdigital.com>
2018-11-29 10:59:43 +0100

    Install Enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../App';
+import { mount } from 'enzyme';
 
 describe('The App', () => {
     it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        mount(<App />);
     });
 });
